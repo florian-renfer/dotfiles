ae8d763f8425ce8e94a2829af133643dd025cdcb
Florian Renfer <florian.renfer@heyoptima.de>
2025-02-09 18:35:37 +0100

    feat: add tmux configuration
    * feat: initialize tmux configuration
    
    * feat: update uninstall.sh permissions
    
    * feat: update theme colors

---
+++ install.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+# Helpers
+default_color=$(tput sgr 0)
+red="$(tput setaf 1)"
+yellow="$(tput setaf 3)"
+green="$(tput setaf 2)"
+blue="$(tput setaf 4)"
+
+info() {
+    printf "%s==> %s%s\n" "$blue" "$1" "$default_color"
+}
+
+success() {
+    printf "%s==> %s%s\n" "$green" "$1" "$default_color"
+}
+
+error() {
+    printf "%s==> %s%s\n" "$red" "$1" "$default_color"
+}
+
+warning() {
+    printf "%s==> %s%s\n" "$yellow" "$1" "$default_color"
+}
+
+# Actual installation process
+printf "\n"
+info "======================"
+info "Creating symlinks..."
+info "======================"
+printf "\n"
+
+ln -s $(pwd)/tmux/tmux.conf $HOME/.config/tmux/tmux.conf
+ln -s $(pwd)/tmux/theme.conf $HOME/.config/tmux/theme.conf
