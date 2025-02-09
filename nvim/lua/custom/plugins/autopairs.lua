056104a62867e405bf9354f57d0d489fe65d54db
Florian Renfer <florian.renfer@heyoptima.de>
2025-02-09 19:02:53 +0100

    feat: enable 'windwp/nvim-autopairs'

--- nvim/lua/custom/plugins/autopairs.lua
+++ nvim/lua/custom/plugins/autopairs.lua
@@ -1,7 +1,7 @@
 return {
-  --   'windwp/nvim-autopairs',
-  --   event = 'InsertEnter',
-  --   config = true,
-  --   opts = {},
+  'windwp/nvim-autopairs',
+  event = 'InsertEnter',
+  config = true,
+  opts = {},
   --   -- this is equivalent to setup({}) function
 }
