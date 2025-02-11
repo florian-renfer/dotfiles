519c9369a5f15a276cc47041f83fb42e0f526fa7
Florian Renfer <florian.renfer@heyoptima.de>
2025-02-11 15:12:41 +0100

    feat: add zsh configuration

--- install.sh
+++ install.sh
@@ -23,6 +23,13 @@ warning() {
     printf "%s==> %s%s\n" "$yellow" "$1" "$default_color"
 }
 
+backup() {
+   warning "A file with the same name exists already, creating backup for: $1" 
+   backup_path="$1.bak"
+   mv "$1" "$backup_path"
+   warning "Backup created: $backup_path"
+}
+
 # Actual installation process
 printf "\n"
 info "======================"
@@ -45,6 +52,10 @@ ln -s $(pwd)/tmux/theme.conf $HOME/.config/tmux/theme.conf
 
 # Neovim
 ln -s $(pwd)/nvim $HOME/.config
+
+# ZSH
+backup $HOME/.zshrc
+ln -s $(pwd)/zsh/.zshrc $HOME/.zshrc
 
 printf "\n"
 info "======================"
