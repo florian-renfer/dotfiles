6a1f778cb413c3e36720b0c65ef93ee423300a92
Renfer, Florian <florian.renfer@heyoptima.de>
2026-01-09 15:08:48 +0100

    feat: current state

--- nvim/lua/plugins/todo-comments.lua
+++ nvim/lua/plugins/todo-comments.lua
@@ -1,8 +1,12 @@
 return {
   "folke/todo-comments.nvim",
-  dependencies = { "nvim-lua/plenary.nvim" },
-  opts = {},
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+    "nvim-telescope/telescope.nvim"
+  },
   keys = {
     { "<leader>st", "<cmd>TodoTelescope<cr>", mode = "n", desc = "[S]earch [T]odos" },
   },
+  opts = {},
+  lazy = false,
 }
