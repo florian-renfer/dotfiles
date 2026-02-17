3e57f4fb18abb3ba6f67b9e498d94807c1889437
Renfer, Florian <florian.renfer@heyoptima.de>
2026-02-17 13:42:17 +0100

    feat(nvim): remove unused plugins, configure copilot

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -1,40 +1,63 @@
 local function filenameFirst(_, path)
-  local tail = vim.fs.basename(path)
-  local parent = vim.fs.dirname(path)
-  if parent == "." then return tail end
-  return string.format("%s\t\t%s", tail, parent)
+	local tail = vim.fs.basename(path)
+	local parent = vim.fs.dirname(path)
+	if parent == "." then
+		return tail
+	end
+	return string.format("%s\t\t%s", tail, parent)
 end
 
 vim.api.nvim_create_autocmd("FileType", {
-  pattern = "TelescopeResults",
-  callback = function(ctx)
-    vim.api.nvim_buf_call(ctx.buf, function()
-      vim.fn.matchadd("TelescopeParent", "\t\t.*$")
-      vim.api.nvim_set_hl(0, "TelescopeParent", { link = "Comment" })
-    end)
-  end,
+	pattern = "TelescopeResults",
+	callback = function(ctx)
+		vim.api.nvim_buf_call(ctx.buf, function()
+			vim.fn.matchadd("TelescopeParent", "\t\t.*$")
+			vim.api.nvim_set_hl(0, "TelescopeParent", { link = "Comment" })
+		end)
+	end,
 })
 
 return {
-  "nvim-telescope/telescope.nvim",
-  tag = "0.1.8",
-  dependencies = { "nvim-lua/plenary.nvim" },
-  opts = {
-    defaults = {
-      path_display = filenameFirst,
-      layout_strategy = "vertical",
-      layout_config = {
-        prompt_position = "bottom",
-        height = 0.95,
-      },
-    },
-  },
-  keys = {
-    { "<leader>sf", "<cmd>:lua require('telescope.builtin').find_files()<cr>",  desc = "[S]earch [f]iles" },
-    { "<leader>sg", "<cmd>:lua require('telescope.builtin').live_grep()<cr>",   desc = "[S]earch via [g]rep" },
-    { "<leader>sh", "<cmd>:lua require('telescope.builtin').help_tags()<cr>",   desc = "[S]earch neovim [h]elp" },
-    { "<leader>sc", "<cmd>:lua require('telescope.builtin').colorscheme()<cr>", desc = "[S]earch [c]olorschemes" },
-    { "<leader>sp", "<cmd>:lua require('telescope.builtin').oldfiles()<cr>",    desc = "[S]earch [p]revious files" },
-    { "<leader>sk", "<cmd>:lua require('telescope.builtin').keymaps()<cr>",     desc = "[S]earch [k]eymaps" },
-  },
+	"nvim-telescope/telescope.nvim",
+	version = "0.2.1",
+	dependencies = {
+		"nvim-lua/plenary.nvim",
+		"nvim-telescope/telescope-ui-select.nvim",
+		"nvim-telescope/telescope-frecency.nvim",
+		{ "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
+	},
+	opts = {
+		defaults = {
+			path_display = filenameFirst,
+			layout_strategy = "vertical",
+			layout_config = {
+				prompt_position = "bottom",
+				height = 0.95,
+			},
+		},
+		extensions = {
+			["ui-select"] = {
+				require("telescope.themes").get_dropdown({}),
+			},
+		},
+	},
+	config = function(_, opts)
+		local telescope = require("telescope")
+		telescope.setup(opts)
+		telescope.load_extension("ui-select")
+		telescope.load_extension("frecency")
+		telescope.load_extension("fzf")
+	end,
+	keys = {
+		{ "<leader>sf", "<cmd>:lua require('telescope.builtin').find_files()<cr>", desc = "[S]earch [f]iles" },
+		{ "<leader>sg", "<cmd>:lua require('telescope.builtin').live_grep()<cr>", desc = "[S]earch via [g]rep" },
+		{ "<leader>sh", "<cmd>:lua require('telescope.builtin').help_tags()<cr>", desc = "[S]earch neovim [h]elp" },
+		{ "<leader>sc", "<cmd>:lua require('telescope.builtin').colorscheme()<cr>", desc = "[S]earch [c]olorschemes" },
+		{
+			"<leader>sp",
+			"<cmd>:lua require('telescope.builtin').oldfiles()<cr>",
+			desc = "[S]earch [p]revious files",
+		},
+		{ "<leader>sk", "<cmd>:lua require('telescope.builtin').keymaps()<cr>", desc = "[S]earch [k]eymaps" },
+	},
 }
