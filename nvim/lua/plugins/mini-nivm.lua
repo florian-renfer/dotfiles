576e77106837dbca043ec50937885d96fd7c9099
Renfer, Florian <florian.renfer@heyoptima.de>
2026-03-15 09:23:12 +0100

    feat(nvim): add mini.nvim

---
+++ nvim/lua/plugins/mini-nivm.lua
@@ -0,0 +1,17 @@
+return {
+	"echasnovski/mini.nvim",
+	config = function()
+		require("mini.ai").setup({})
+		require("mini.comment").setup({})
+		-- TODO not oworking at the moment, re-check configuration in keys.lua and opts.lua
+		require("mini.move").setup({})
+		require("mini.surround").setup({})
+		require("mini.cursorword").setup({})
+		require("mini.indentscope").setup({})
+		require("mini.pairs").setup({})
+		require("mini.trailspace").setup({})
+		require("mini.bufremove").setup({})
+		require("mini.notify").setup({})
+		require("mini.icons").setup({})
+	end,
+}
