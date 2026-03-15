c08f8e3edd3a8622dee9809910213a9594d1c575
Renfer, Florian <florian.renfer@heyoptima.de>
2026-03-15 08:09:01 +0100

    feat(nvim): add cmd to gitsigns

--- nvim/lua/plugins/gitsigns.lua
+++ nvim/lua/plugins/gitsigns.lua
@@ -1,16 +1,17 @@
 return {
-  'lewis6991/gitsigns.nvim',
-  opts = {
-    current_line_blame = true,
-  },
-  keys = {
-    {
-      "<leader>gb",
-      function()
-        require("gitsigns").toggle_current_line_blame()
-      end,
-      mode = "n",
-      desc = "[G]it [b]lame line",
-    },
-  },
+	"lewis6991/gitsigns.nvim",
+	cmd = { "Gitsigns" },
+	opts = {
+		current_line_blame = true,
+	},
+	keys = {
+		{
+			"<leader>gb",
+			function()
+				require("gitsigns").toggle_current_line_blame()
+			end,
+			mode = "n",
+			desc = "[G]it [b]lame line",
+		},
+	},
 }
